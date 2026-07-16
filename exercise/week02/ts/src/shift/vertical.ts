3b23290a4fa49463237c4f6a10e49457ec09d106
AdeulianSuke <>
2026-07-16 15:46:07 +0200

    feat: implement submarine logic for week02 in C#, Java, PHP and TS

--- exercise/week02/ts/src/shift/vertical.ts
+++ exercise/week02/ts/src/shift/vertical.ts
@@ -1,4 +1,4 @@
-export class Vertical {
+/*export class Vertical {
     static whichFloor(instructions: string): number {
         let val: Array<[string, number]> = [];
 
@@ -21,5 +21,35 @@ export class Vertical {
         }
 
         return result;
+    }
+}*/
+
+const UP_SIGNAL = '(';
+const DOWN_SIGNAL = ')';
+const ANOMALY_SIGNAL = '🧝';
+
+export class Vertical {
+    public static whichFloor(signalStream: string): number {
+        return signalStream.includes(ANOMALY_SIGNAL)
+            ? Vertical.whichFloorUnderAnomaly(signalStream)
+            : Vertical.whichFloorNormally(signalStream);
+    }
+
+    private static whichFloorNormally(signalStream: string): number {
+        let floor = 0;
+        for (const signal of signalStream) {
+            if (signal === UP_SIGNAL) floor += 1;
+            else if (signal === DOWN_SIGNAL) floor -= 1;
+        }
+        return floor;
+    }
+
+    private static whichFloorUnderAnomaly(signalStream: string): number {
+        let floor = 0;
+        for (const signal of signalStream) {
+            if (signal === DOWN_SIGNAL) floor += 3;
+            else if (signal === UP_SIGNAL) floor -= 2;
+        }
+        return floor;
     }
 }
--- exercise/week02/ts/tests/vertical.shift.spec.ts
+++ exercise/week02/ts/tests/vertical.shift.spec.ts
@@ -1,5 +1,5 @@
 import * as fs from "node:fs";
-import {Vertical} from "../src/shift/vertical";
+import { Vertical } from "../src/shift/vertical";
 import * as path from "node:path";
 
 describe('Vertical Shift', () => {
